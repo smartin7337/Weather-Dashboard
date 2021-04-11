48160aa1976a80ccd1bb81e3a51bbb0d928251ac
Vossie <sv7337@outlook.com>
2021-04-11 17:39:07 -0400

    setting up js, establishing global variables and local storage functions

---
+++ script.js
@@ -0,0 +1,23 @@
+  
+// local storage functions
+initCityList();
+initWeather();
+
+// global variable declarations
+var cityList = [];
+var cityname;
+
+
+// displays the city entered into the DOM
+function renderCities(){
+    $("#cityList").empty();
+    $("#cityInput").val("");
+    
+    for (i=0; i<cityList.length; i++){
+        var a = $("<a>");
+        a.addClass("list-group-item list-group-item-action list-group-item-primary city");
+        a.attr("data-name", cityList[i]);
+        a.text(cityList[i]);
+        $("#cityList").prepend(a);
+    } 
+}
